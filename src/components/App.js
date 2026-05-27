c42cdb1a9923aa49abf41ef18e8e3cd76e862c8b
Akash <akashpillai40@gmail.com>
2026-05-27 20:11:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      {name && <h1>Hello, {name}!</h1>}
+    </div>
+  );
+}
+
+export default App;
